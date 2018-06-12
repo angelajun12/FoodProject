d0ddd851396e58d7715f3b1dd7d7dd1babf11f96
Angela (Inhea) Jun <angela.jun12@gmail.com>
2018-06-12 17:18:27 -0400

    created VCclasses appDelegate configure firebase introduce buttons to authentication functions

---
+++ FoodProject/Controller/SignUpVC.swift
@@ -0,0 +1,42 @@
+//
+//  SignUpVC.swift
+//  FoodProject
+//
+//  Created by Angela Jun on 6/12/18.
+//  Copyright © 2018 Angela Jun. All rights reserved.
+//
+
+import UIKit
+import FirebaseAuth
+class SignUpVC: UIViewController {
+
+    @IBOutlet weak var Email: UITextField!
+    @IBOutlet weak var Password: UITextField!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func SignUpPressed(_ sender: Any) {
+    Auth.auth().createUser(withEmail: Email.text!, password: Password.text!, completion: <#T##AuthDataResultCallback?##AuthDataResultCallback?##(AuthDataResult?, Error?) -> Void#>)
+    
+    }
+    
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
